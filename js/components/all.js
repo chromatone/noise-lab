96d4d2647a5ae607990732926e1311572f283f68
DeFUCC <fond@fondfrukt.ru>
2020-04-10 21:11:18 +0300

    more space and no overflow scroll

--- js/components/all.js
+++ js/components/all.js
@@ -1,3 +1,4 @@
 export {autoFilter} from './auto-filter.js'
 export {autoPanner} from './auto-panner.js'
 export {noiseGenerator} from './noise-generator.js'
+export {bitCrusher} from './bit-crusher.js'
